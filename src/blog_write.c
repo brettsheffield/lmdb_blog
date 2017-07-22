b2c5864d1d4a0541c7ab74cd18dafe678dc9d049
Brett Sheffield <brett@gladserv.com>
2017-07-22 21:32:24 +0100

    only free() when needed

--- src/blog_write.c
+++ src/blog_write.c
@@ -66,7 +66,8 @@ int main(int argc, char **argv)
 
 	printf("writing '%s' to key '%s'\n", data, argv[1]);
 	rc = blog_write_newentry(argv[1], data);
-	free(data);
+	if (argc == 2)
+		free(data);
 
 	return rc;
 }
