2ef3ee514eff7b45dab2c7e008be02ed49b9d9ac
Brett Sheffield <brett@gladserv.com>
2017-07-22 16:56:12 +0100

    blog_write_newentry()

---
+++ src/blog_common.h
@@ -0,0 +1,13 @@
+#include <lmdb.h>
+#include <stdio.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <stdlib.h>
+
+#define DB_PATH "/var/cache/blog/"
+
+#define E(expr) CHECK((rc = (expr)) == MDB_SUCCESS, #expr)
+#define RES(err, expr) ((rc = expr) == (err) || (CHECK(!rc, #expr), 0))
+#define CHECK(test, msg) ((test) ? (void)0 : ((void)fprintf(stderr, \
+        "%s:%d: %s: %s\n", __FILE__, __LINE__, msg, mdb_strerror(rc)), abort()))
+
--- src/blog_write.c
+++ src/blog_write.c
@@ -1,4 +1,26 @@
-int main()
+#include "blog_common.h"
+
+int blog_write_newentry(char *key, char *value)
 {
+	int rc;
+
+	MDB_env *env;
+	MDB_txn *txn;
+
+	E(mdb_env_create(&env));
+	E(mdb_env_open(env, DB_PATH, MDB_FIXEDMAP, S_IRWXU));
+
+	E(mdb_txn_begin(env, NULL, 0, &txn));
+
+	E(mdb_txn_commit(txn));
+
+	mdb_env_close(env);
 	return 0;
+}
+
+int main(int argc, char **argv)
+{
+	if (argc == 3)
+		return blog_write_newentry(argv[1], argv[2]);
+	return -1;
 }
