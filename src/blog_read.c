3400446413f9ebe84a973e862ff9e69037844632
Brett Sheffield <brett@gladserv.com>
2017-07-22 18:48:45 +0100

    read and write functioning

--- src/blog_read.c
+++ src/blog_read.c
@@ -9,19 +9,19 @@ int blog_read_getentry(char *k, char **v)
 	MDB_dbi dbi;
 	MDB_val key, data;
 
-	key.mv_size = sizeof(k);
+	key.mv_size = strlen(k) + 1;
 	key.mv_data = k;
 
 	E(mdb_env_create(&env));
-	E(mdb_env_open(env, DB_PATH, MDB_FIXEDMAP, S_IRWXU));
+	E(mdb_env_open(env, DB_PATH, 0, S_IRWXU));
 	E(mdb_txn_begin(env, NULL, MDB_RDONLY, &txn));
 	E(mdb_dbi_open(txn, NULL, 0, &dbi));
 
 	E(mdb_get(txn, dbi, &key, &data));
-
-	*v = data.mv_data;
-
 	mdb_txn_abort(txn);
+
+	*v = strdup(data.mv_data);
+
 	mdb_dbi_close(env, dbi);
 	mdb_env_close(env);
 	return 0;
@@ -33,6 +33,7 @@ int main(int argc, char **argv)
 	if (argc == 2) {
 		blog_read_getentry(argv[1], &data);
 		puts(data);
+		free(data);
 		return 0;
 	}
 
--- src/blog_write.c
+++ src/blog_write.c
@@ -8,28 +8,32 @@ int blog_write_newentry(char *k, char *v)
 	MDB_txn *txn;
 	MDB_dbi dbi;
 	MDB_val key, data;
+	MDB_stat mst;
 
-	key.mv_size = sizeof(k);
+	key.mv_size = strlen(k) + 1;
 	key.mv_data = k;
 	data.mv_size = sizeof(v);
 	data.mv_data = v;
 
 	E(mdb_env_create(&env));
-	E(mdb_env_open(env, DB_PATH, MDB_FIXEDMAP, S_IRWXU));
+	E(mdb_env_open(env, DB_PATH, 0, S_IRWXU));
 	E(mdb_txn_begin(env, NULL, 0, &txn));
 	E(mdb_dbi_open(txn, NULL, 0, &dbi));
 
-	mdb_put(txn, dbi, &key, &data, MDB_NOOVERWRITE);
+	mdb_put(txn, dbi, &key, &data, 0);
 
 	E(mdb_txn_commit(txn));
+	E(mdb_env_stat(env, &mst));
+
 	mdb_dbi_close(env, dbi);
 	mdb_env_close(env);
+
 	return 0;
 }
 
 int main(int argc, char **argv)
 {
-	puts(argv[2]);
+	printf("writing '%s' to key '%s'\n", argv[2], argv[1]);
 	if (argc == 3)
 		return blog_write_newentry(argv[1], argv[2]);
 	return -1;
