c44ff25cac34f2f59addb3b63000daa44c0fa093
Brett Sheffield <brett@gladserv.com>
2017-08-02 20:15:55 +0100

    add LICENSE

--- src/blog_common.h
+++ src/blog_common.h
@@ -1,3 +1,25 @@
+/*
+ * blog_common.h
+ *
+ * this file is part of lmdb_blog
+ *
+ * Copyright (c) 2017 Brett Sheffield <brett@gladserv.com>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program (see the file COPYING in the distribution).
+ * If not, see <http://www.gnu.org/licenses/>.
+ */
+
 #include <lmdb.h>
 #include <stdio.h>
 #include <sys/types.h>
@@ -8,8 +30,10 @@
 
 #define DB_PATH "/var/cache/blog/blog.mdb"
 
+/* These 3 lines from lmdb sources
+ * Copyright 2011-2017 Howard Chu, Symas Corp
+ * http://www.OpenLDAP.org/license.html */
 #define E(expr) CHECK((rc = (expr)) == MDB_SUCCESS, #expr)
 #define RES(err, expr) ((rc = expr) == (err) || (CHECK(!rc, #expr), 0))
 #define CHECK(test, msg) ((test) ? (void)0 : ((void)fprintf(stderr, \
         "%s:%d: %s: %s\n", __FILE__, __LINE__, msg, mdb_strerror(rc)), abort()))
-
